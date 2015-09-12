3382dea9ba6a1fd1f225f6904a4b769ffa474c10
rondreas <andreas.ranman@gmail.com>
2015-09-12 21:52:32 +0200

    Forgot ending quotes.

--- makelinks.cmd
+++ makelinks.cmd
@@ -1,3 +1,3 @@
-mklink "%userprofile%\_gvimrc" "%userprofile%\Documents\GitHub\dotfiles\vim\.gvimrc
-mklink "%userprofile%\_vimrc" "%userprofile%\Documents\GitHub\dotfiles\vim\.vimrc
-mklink /d "%userprofile%\vimfiles" "%userprofile%\Documents\GitHub\dotfiles\vim\.vim
+mklink "%userprofile%\_gvimrc" "%userprofile%\Documents\GitHub\dotfiles\vim\.gvimrc"
+mklink "%userprofile%\_vimrc" "%userprofile%\Documents\GitHub\dotfiles\vim\.vimrc"
+mklink /d "%userprofile%\vimfiles" "%userprofile%\Documents\GitHub\dotfiles\vim\.vim"
