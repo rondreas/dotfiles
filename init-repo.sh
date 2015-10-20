c4443c1179c92658411b8b9e6996467b97a376af
rondreas <andreas.ranman@gmail.com>
2015-10-20 23:29:25 +0200

    Adding vim-surround.

--- init-repo.sh
+++ init-repo.sh
@@ -3,3 +3,4 @@
 git submodule update --init vim/.vim/bundle/nerdtree
 git submodule update --init vim/.vim/bundle/vim-colors-solarized
 git submodule update --init vim/.vim/bundle/pydoc
+git submodule update --init vim/.vim/bundle/vim-surround
