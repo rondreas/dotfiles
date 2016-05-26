8cb6638f322a9d065fb3935c6993efbc28c79ef9
andreas rånman <andreas.ranman@gmail.com>
2016-05-26 12:57:34 +0200

    Adding script for removing previously installed submodules for Pathogen.

---
+++ rm_submodules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git submodule deinit vim/.vim/bundle/nerdtree
+git rm vim/.vim/bundle/nerdtree
+git submodule deinit vim/.vim/bundle/vim-colors-solarized
+git rm vim/.vim/bundle/vim-colors-solarized
+git submodule deinit vim/.vim/bundle/pydoc
+git rm vim/.vim/bundle/pydoc
+git submodule deinit vim/.vim/bundle/vim-surround
+git rm vim/.vim/bundle/vim-surround
